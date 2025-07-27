955d4b19e0169217aec88681cbf3d4a4a7e102c7
Ayushgangwar02 <ayushgangwar762@gmail.com>
2025-07-27 12:57:58 +0530

    created the view trip page

--- AI_Travel_Planner/src/main.jsx
+++ AI_Travel_Planner/src/main.jsx
@@ -7,6 +7,7 @@ import CreateTrip from './create-trip/index.jsx'
 import Header from './components/custom/Header.jsx'
 import { Toaster } from '@/components/ui/sonner'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import ViewTrip from './view-trip/[tripId]/index.jsx'
 const router = createBrowserRouter([
   {
     path: "/",
@@ -23,6 +24,15 @@ const router = createBrowserRouter([
       <>
         <Header/>
         <CreateTrip/>
+      </>
+    )
+  },
+  {
+    path: '/view-trip/:tripId',
+    element: (
+      <>
+        <Header/>
+        <ViewTrip/>
       </>
     )
   }
