4ce88af9ed306cee8bec739ae6df39d4bf5a0c06
Ayushgangwar02 <ayushgangwar762@gmail.com>
2025-07-30 15:36:37 +0530

    Fix Get Started button clickability by adjusting z-index layering

--- AI_Travel_Planner/src/components/custom/Hero.jsx
+++ AI_Travel_Planner/src/components/custom/Hero.jsx
@@ -9,11 +9,11 @@ function Hero() {
         className='font-extrabold text-[40px] text-center mt-10'
         ><span className='text-[#f56551]'>Discover Your Next Adventure with Al:</span> Personalized Itineraries at Your Fingertips</h1>
         <p className='text-xl text-gray-500 text-center'>Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget.</p>
-        <Link to = {'/create-trip'}>
+        <Link to = {'/create-trip'} className="z-10 relative">
             <Button>Get Started, It's Free</Button>
         </Link>
 
-        <img src="/homePage.png" className='-mt-20'/>
+        <img src="/homePage.png" className='-mt-20 relative z-0'/>
           
     </div>
 
